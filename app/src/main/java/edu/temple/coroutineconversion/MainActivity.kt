578c59ab7dc3957b4e67ab0ca744085651ed049b
D4RY-exe <129776733+D4RY-exe@users.noreply.github.com>
2025-04-02 15:38:30 -0400

    initial code

--- app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
+++ app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
@@ -21,24 +21,23 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.currentTextView)
     }
 
-    val handler = Handler(Looper.getMainLooper(), Handler.Callback {
-
-        currentTextView.text = String.format(Locale.getDefault(), "Current opacity: %d", it.what)
-        cakeImageView.alpha = it.what / 100f
-        true
-    })
-
+    //val handler = Handler(Looper.getMainLooper(), Handler.Callback {
+    //    currentTextView.text = String.format(Locale.getDefault(), "Current opacity: %d", it.what)
+    //    cakeImageView.alpha = it.what / 100f
+    //    true
+    //})
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         findViewById<Button>(R.id.revealButton).setOnClickListener{
-            Thread{
-                repeat(100) {
-                    handler.sendEmptyMessage(it)
-                    Thread.sleep(40)
+            lifecycleScope.launch {
+                repeat(100){opacityValue ->
+                    currentTextView.text = String.format (Locale.getDefault(), "Current opacity: %d", opacityValue)
+                    cakeImageView.alpha = opacityValue / 100f
+                    delay(40)
                 }
-            }.start()
+            }
         }
     }
 }
